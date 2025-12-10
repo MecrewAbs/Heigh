8133e6ec9835efbf4317f12e186ed2feda005d1b
Andrey <bakrovee@gmail.com>
2025-12-10 22:19:14 +0300

    Complete

--- main.go
+++ main.go
@@ -2,6 +2,11 @@ package main
 
 import "fmt"
 
+const UsdToRub float64 = 77.9
+const UsdToEur float64 = 0.856
+
 func main() {
-	fmt.Println("hello world")
+	fmt.Printf("%f\n", UsdToRub)
+	fmt.Printf("%f\n", UsdToEur)
+	fmt.Println("EurToRub = ", UsdToRub/UsdToEur)
 }
